656373fefe0482634bd464c7a6be72362c4b0485
Nicolas Rémond <nremond+github@gmail.com>
2013-12-13 10:14:48 +0100

    formatting

--- examples/src/main/scala/io/segl/MacroExamples.scala
+++ examples/src/main/scala/io/segl/MacroExamples.scala
@@ -7,21 +7,21 @@ import org.slf4j.LoggerFactory
 
 object MacroExamples {
 
-private val logger = LoggerFactory.getLogger("MacroProlo")
+  private val logger = LoggerFactory.getLogger("MacroProlo")
 
 
   def main(args: Array[String]) {
 
     // ex1
-  	hello("nico")
+    hello("nico")
 
     // ex2
     val a = 1
     val b = 5
-    trace (a+b)
+    trace(a + b)
 
 
     // ex3
-    info(a+b)
+    info(a + b)
   }
 }
